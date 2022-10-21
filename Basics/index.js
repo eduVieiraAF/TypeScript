1995fe073a1598b24a95de6236afa3df1b2a5ad0
Eduardo Vieira <edu_vieira@hotmail.com>
2022-10-21 17:24:35 -0300

    TS Beginning

---
+++ Basics/index.js
@@ -0,0 +1,13 @@
+//* TYPESCRIPT
+/*
+    → is not a language.
+    → is an extension to JavaScript both run simultaneously
+    → deals with data types and addresses other shortcomings of JavaScript
+    → shows errors not in runtime, so you can see them earlier
+    → needs to be transpiled into a JavaScript file
+    → helps you write better code
+    → adds static typing for type checking
+    → refactoring
+    → Shorthand notations
+    → contains everything JavaScript has
+*/
---
+++ Basics/src/index.ts
@@ -0,0 +1,24 @@
+//* TYPESCRIPT
+/*
+    → is not a language.
+    → is an extension to JavaScript both run simultaneously
+    → deals with data types and addresses other shortcomings of JavaScript
+    → shows errors not in runtime, so you can see them earlier
+    → needs to be transpiled into a JavaScript file
+    → helps you write better code
+    → adds static typing for type checking
+    → refactoring
+    → Shorthand notations
+    → contains everything JavaScript has
+*/
+
+let age: number = 20;
+
+if (age >= 18 && age <= 50) console.log("An adult")
+else if (age < 18 && age >= 11) console.log("The jerk years")
+else if (age < 11) console.log("Keep your nose clean and diaper dry.")
+else console.log("Age's come")
+
+
+
+
