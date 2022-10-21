55efde054809be510b39855e04647b1bab33b021
Eduardo Vieira <edu_vieira@hotmail.com>
2022-10-21 19:08:54 -0300

    Built-in types
    special attention to any

--- Basics/dist/index.js
+++ Basics/dist/index.js
@@ -1,11 +1,2 @@
 "use strict";
-let age = 20;
-if (age >= 50)
-    console.log("Old fart");
-else if (age < 50 && age >= 18)
-    console.log("A grownup");
-else if (age < 18 && age >= 12)
-    console.log("Ah, the jerk years");
-else
-    console.log("Keep your nose clean");
 //# sourceMappingURL=index.js.map
--- Basics/src/index.ts
+++ Basics/src/index.ts
@@ -10,7 +10,6 @@
     → refactoring
     → Shorthand notations
     → contains everything JavaScript has
-*/
 
 let age: number = 20;
 
@@ -18,6 +17,41 @@ if (age >= 50) console.log("Old fart");
 else if(age < 50 && age >= 18) console.log("A grownup");
 else if (age < 18 && age >= 12) console.log("Ah, the jerk years");
 else console.log("Keep your nose clean");
+
+*/
+
+//* Built-in Types
+
+//* JavaScript      ||  TypeScript (adds more)
+/*                  ||  
+    → number        ||  → any
+    → string        ||  → unkonw
+    → bolean        ||  → neverr
+    → null          ||  → enum
+    → undefined     ||  → tuple
+    → object        ||
+
+let id: number = 164_870;
+let course: string = 'TypeScript';
+let isPublished = true; // doesnt necessarily need annotation
+let grade; // type any
+
+//* type any will let you reassign using different types
+grade = 10;
+grade = "B+"; 
+
+function render(document: any) { // needs annotation when it's a parameter
+    console.log(document)
+}
+
+//* in tsconfig.json you can enable (// "noImplicitAny": true,) and set it to false, but it's not recommended
+
+*/
+
+
+
+
+
 
 
 
