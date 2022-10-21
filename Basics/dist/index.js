4f14c63ec873f127e643ec882a8e2b184c9fc9e8
Eduardo Vieira <edu_vieira@hotmail.com>
2022-10-21 18:53:57 -0300

    TS debugging

--- Basics/dist/index.js
+++ Basics/dist/index.js
@@ -1,7 +1,11 @@
 "use strict";
 let age = 20;
-if (age >= 18) {
-    age = age + 25;
-}
-console.log(age);
+if (age >= 50)
+    console.log("Old fart");
+else if (age < 50 && age >= 18)
+    console.log("A grownup");
+else if (age < 18 && age >= 12)
+    console.log("Ah, the jerk years");
+else
+    console.log("Keep your nose clean");
 //# sourceMappingURL=index.js.map
--- Basics/src/index.ts
+++ Basics/src/index.ts
@@ -14,11 +14,13 @@
 
 let age: number = 20;
 
-if (age < 50) {
-    age = age + 25;
-}
+if (age >= 50) console.log("Old fart");
+else if(age < 50 && age >= 18) console.log("A grownup");
+else if (age < 18 && age >= 12) console.log("Ah, the jerk years");
+else console.log("Keep your nose clean");
 
-console.log(age)
+
+
 
 
 
