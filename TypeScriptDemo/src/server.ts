45b205a12e6cb9a1bc38ca17d18345a3645114e1
Eduardo Vieira <edu_vieira@hotmail.com>
2022-11-18 20:27:55 -0300

    Dev

--- TypeScriptDemo/src/server.ts
+++ TypeScriptDemo/src/server.ts
@@ -1,2 +1,9 @@
-import express from 'express';
+import express from "express";
 
+const app = express();
+
+app.get('/', (req, res) => {
+    res.send("Hello, world with real-time transpile 2");
+});
+
+app.listen(3000, () => console.log("Server is listening..."))
