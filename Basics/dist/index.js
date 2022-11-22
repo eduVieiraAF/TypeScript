be9298841c0293ae8779b3ca12264de754764603
Eduardo Vieira <edu_vieira@hotmail.com>
2022-11-22 06:44:49 -0300

    Generics

--- Basics/dist/index.js
+++ Basics/dist/index.js
@@ -1,8 +1,119 @@
 "use strict";
 var _a;
+let age = 20;
+if (age >= 50)
+    console.log("Old fart");
+else if (age < 50 && age >= 18)
+    console.log("A grownup");
+else if (age < 18 && age >= 12)
+    console.log("Ah, the jerk years");
+else
+    console.log("Keep your nose clean");
+let id = 164870;
+let course = "TypeScript";
+let isPublished = true;
+let grade;
+grade = 10;
+grade = "B+";
+function render(document) {
+    console.log(document);
+}
+let numbers = [1, 2, 3];
+numbers.forEach((n) => console.log(n));
+let user = [40, "Edu"];
+console.log(user);
+console.log(user[1]);
+var Size;
+(function (Size) {
+    Size[Size["Small"] = 1] = "Small";
+    Size[Size["Mediuam"] = 2] = "Mediuam";
+    Size[Size["Large"] = 3] = "Large";
+})(Size || (Size = {}));
+console.log(Size);
+let tShirt = ["Yellow", Size.Large];
+tShirt.forEach((n) => console.log(n));
+let httpResponse = 4;
+switch (httpResponse) {
+    case 4:
+        console.log("Connected");
+        break;
+    case 3:
+        console.log("Permission denied");
+        break;
+    case 2:
+        console.log("Unable to connect");
+        break;
+    case 1:
+        console.log("UNKNOWN ERROR");
+}
+function calculateTax(income) {
+    if (income > 50000)
+        return income - income * 0.8;
+    return income - income * 0.7;
+}
+let pay = calculateTax(82000);
+console.log(pay);
+let employeeSimple = {
+    id: 1,
+    name: "Edu",
+    retire: (date) => {
+        console.log(date);
+    },
+};
+let employee = {
+    id: 1,
+    name: "Edu",
+    retire: (date) => console.log(date),
+};
+console.log(employee);
+function kgToLbs(weight) {
+    if (typeof weight === "number")
+        return weight * 2.2;
+    else
+        return parseInt(weight) * 2.2;
+}
+console.log(kgToLbs(25));
+console.log(kgToLbs("25kg"));
+let weight;
+let button = {
+    drag: () => { },
+    resize: () => { },
+};
+let quantity1;
+let quantity = 100;
+function greet(name) {
+    if (name)
+        console.log(name.toUpperCase());
+    else
+        console.log("Empty or null.");
+}
+greet(null);
 function getCustomer(id) {
     return id === 0 ? null : { birthday: new Date() };
 }
 let customer = getCustomer(2);
 console.log((_a = customer === null || customer === void 0 ? void 0 : customer.birthday) === null || _a === void 0 ? void 0 : _a.getFullYear());
+function createPairs(v1, v2) {
+    return [v1, v2];
+}
+console.log(createPairs("Hello", 500));
+class NamedValue {
+    constructor(name) {
+        this.name = name;
+    }
+    setValue(value) {
+        this._value = value;
+    }
+    getValue() {
+        return this._value;
+    }
+    toString() {
+        return `${this.name}: ${this._value}`;
+    }
+}
+const value1 = new NamedValue('myNumber');
+value1.setValue(613);
+const value2 = new NamedValue('userName');
+value2.setValue("ZeroCool");
+console.log(`${value1.toString()} → ${value2.toString()}`);
 //# sourceMappingURL=index.js.map
--- Basics/src/index.ts
+++ Basics/src/index.ts
@@ -15,11 +15,9 @@
 let age: number = 20;
 
 if (age >= 50) console.log("Old fart");
-else if(age < 50 && age >= 18) console.log("A grownup");
+else if (age < 50 && age >= 18) console.log("A grownup");
 else if (age < 18 && age >= 12) console.log("Ah, the jerk years");
 else console.log("Keep your nose clean");
-
-
 
 //* Built-in Types
 
@@ -34,27 +32,28 @@ else console.log("Keep your nose clean");
 */
 
 let id: number = 164_870;
-let course: string = 'TypeScript';
+let course: string = "TypeScript";
 let isPublished = true; // doesnt necessarily need annotation
 let grade; // type any
 
 //* type any will let you reassign using different types
 grade = 10;
-grade = "B+"; 
+grade = "B+";
 
-function render(document: any) { // needs annotation when it's a parameter
-    console.log(document);
+function render(document: any) {
+  // needs annotation when it's a parameter
+  console.log(document);
 }
 
 //* in tsconfig.json you can enable (// "noImplicitAny": true,) and set it to false, but it's not recommended
 
 //* Arrays
 // let numbers: number[] = [1, 2, 3, 'four', '5'];  // to avoid iteration problems, it's recommended to annotate arrays
-                                                    //especially if you're declaring an empty array
+//especially if you're declaring an empty array
 
-let numbers: number[] = [1, 2, 3, ];
+let numbers: number[] = [1, 2, 3];
 
-numbers.forEach(n => console.log(n));
+numbers.forEach((n) => console.log(n));
 
 //* Tuples
 let user: [number, string] = [40, "Edu"]; // avoid using more than 2 elements
@@ -63,109 +62,108 @@ console.log(user[1]);
 
 //* Enums
 enum Size {
-    Small = 1, // Other will be automatically add
-    Mediuam,
-    Large
+  Small = 1, // Other will be automatically add
+  Mediuam,
+  Large,
 }
 
-console.log(Size) // {1: 'Small', 2: 'Mediuam', 3: 'Large', Small: 1, Mediuam: 2, Large: 3}
-let tShirt = ["Yellow", Size.Large]
+console.log(Size); // {1: 'Small', 2: 'Mediuam', 3: 'Large', Small: 1, Mediuam: 2, Large: 3}
+let tShirt = ["Yellow", Size.Large];
 
-tShirt.forEach(n => console.log(n))
+tShirt.forEach((n) => console.log(n));
 
 const enum NetworkResponse { // optmized transpiling
-    Success = "Connected",
-    Error = "Unable to connect",
-    Blocked = "Permission denied",
-    Unkown = "UNKNOWN ERROR"
+  Success = "Connected",
+  Error = "Unable to connect",
+  Blocked = "Permission denied",
+  Unkown = "UNKNOWN ERROR",
 }
 
-let httpResponse = 4
+let httpResponse = 4;
 
 switch (httpResponse) {
-    case 4:
-        console.log(NetworkResponse.Success);
-        break;
-    case 3:
-        console.log(NetworkResponse.Blocked);
-        break;
-    case 2:
-        console.log(NetworkResponse.Error);
-        break;
-    case 1:
-        console.log(NetworkResponse.Unkown);
+  case 4:
+    console.log(NetworkResponse.Success);
+    break;
+  case 3:
+    console.log(NetworkResponse.Blocked);
+    break;
+  case 2:
+    console.log(NetworkResponse.Error);
+    break;
+  case 1:
+    console.log(NetworkResponse.Unkown);
 }
 
 //* Functions
-function calculateTax(income: number): number { // return type
-    if (income > 50_000) return income - (income * 0.8);
-    
-    return income - (income * 0.7);
+function calculateTax(income: number): number {
+  // return type
+  if (income > 50_000) return income - income * 0.8;
+
+  return income - income * 0.7;
 }
 
+let pay = calculateTax(82_000);
+console.log(pay);
 
-let pay = calculateTax(82_000)
-console.log(pay)
- 
+//* Objects
 
-//* Objects 
-
-let employeeSimple : {
-    readonly id: number,
-    name: string,
-    age?: number,
-    retire: (date: Date) => void
-} = { 
-    id:1, 
-    name:"Edu",
-    retire: (date: Date) => {
-        console.log(date)
-    }
+let employeeSimple: {
+  readonly id: number;
+  name: string;
+  age?: number;
+  retire: (date: Date) => void;
+} = {
+  id: 1,
+  name: "Edu",
+  retire: (date: Date) => {
+    console.log(date);
+  },
 };
 
 type Empolyee = {
-    readonly id: number,
-    name: string,
-    age?: number,
-    retire: (date: Date) => void
-}
+  readonly id: number;
+  name: string;
+  age?: number;
+  retire: (date: Date) => void;
+};
 
 let employee: Empolyee = {
-    id: 1,
-    name: "Edu",
-    retire: (date: Date) => console.log(date)
-}
+  id: 1,
+  name: "Edu",
+  retire: (date: Date) => console.log(date),
+};
 
-console.log(employee); 
+console.log(employee);
 
 //* Union types gives a variable or function param different types
 function kgToLbs(weight: number | string): number {
-    // narrowing
-    if (typeof weight === 'number') return weight * 2.2;
-    else return parseInt(weight) * 2.2;
+  // narrowing
+  if (typeof weight === "number") return weight * 2.2;
+  else return parseInt(weight) * 2.2;
 }
 
 console.log(kgToLbs(25));
-console.log(kgToLbs('25kg'));
+console.log(kgToLbs("25kg"));
 
 //* Intersection Types
 let weight: number & string; // both types but it's practically not a good idea
 
 // Unite them as the following example
 type Draggable = {
-    drag: () => void
+  drag: () => void;
 };
 
 type Resizable = {
-    resize: () => void
+  resize: () => void;
 };
 
 type UIWidget = Draggable & Resizable;
 
 let button: UIWidget = {
-    drag: () => {},
-    resize: () => {},
-}
+  drag: () => {},
+  resize: () => {},
+};
 
 //* Literal Types (exact, specific)
 let quantity1: 50 | 100; // can be set to 50 or 100
@@ -173,30 +171,62 @@ let quantity1: 50 | 100; // can be set to 50 or 100
 type Quantity = 50 | 100;
 let quantity: Quantity = 100;
 
-type Metrics = 'cm' | 'inch' | 'yrd';
+type Metrics = "cm" | "inch" | "yrd";
 
 //* Nullable Types
-function greet(name:string | null | undefined) {
-    if (name) console.log(name.toUpperCase());
-    else console.log("Empty or null.");
+function greet(name: string | null | undefined) {
+  if (name) console.log(name.toUpperCase());
+  else console.log("Empty or null.");
 }
 
 greet(null);
 
-
 //* Optional chaining
 type Customer = {
-    birthday?: Date
+  birthday?: Date;
 };
 
 function getCustomer(id: number): Customer | null | undefined {
-    return id === 0 ? null: { birthday: new Date() };
+  return id === 0 ? null : { birthday: new Date() };
 }
 
 let customer = getCustomer(2);
-console.log(customer?.birthday?.getFullYear())
+console.log(customer?.birthday?.getFullYear());
 
 //* Optional element access operator → useful with array
 // customers?.[0];
 
+//* Basic Generics
+function createPairs<str, num>(v1: str, v2: num): [str, num] {
+    return [v1, v2];
+}
 
+console.log(createPairs<string, number>("Hello", 500));
+
+//* Generics → Classes
+
+class NamedValue<T> {
+    private _value: T | undefined;
+
+    constructor(private name: string) {}
+
+    public setValue(value:T) {
+        this._value = value;
+    }
+
+    public getValue(): T | undefined {
+        return this._value;
+    }
+
+    public toString(): string {
+        return `${this.name}: ${this._value}`
+    }
+}
+
+const value1 = new NamedValue<number>('myNumber');
+value1.setValue(613);
+
+const value2 = new NamedValue<string>('userName');
+value2.setValue("ZeroCool");
+
+console.log(`${value1.toString()} → ${value2.toString()}`)
